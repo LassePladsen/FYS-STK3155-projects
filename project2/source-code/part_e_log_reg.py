ac244c3167df43e902dcc23c30c17f58711e8c48
Rogumann <lasse.pladsen@hotmail.com>
2023-11-08 14:06:19 +0100

    ...

--- project2/source-code/part_e_log_reg.py
+++ project2/source-code/part_e_log_reg.py
@@ -1,37 +1,49 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from jax import grad  # automatic differentitation
 
 from part_b_activation import sigmoid
+from part_b_cost import cost_logreg
 
 # Parameters
 n = 100  # no. data points
 noise_std = 1  # standard deviation of noise
 xmax = 5  # max x value
-lmbda = 0.0001  # shrinkage  hyperparameter lambda
+lmbda = 0.0001  # shrinkage hyperparameter lambda
 eta = 0.001  # learning rate
 n_batches = 3  # no. minibatches for sgd
-M = int(n/n_batches)
 degree = 2  # max polynomial degree for design matrix
-n_epochs = 1000  # no. epochs/iterations for nn training
-rng_seed = 2023  # seed for generating psuedo-random values, helps withbugging purposes
+n_epochs = 1000  # max no. epochs/iterations for nn training
+theta_tol = 1e-7  # tolerance for convergence check
+rng_seed = 2023  # seed for generating psuedo-random values, helps with debugging purposes
 
-
-# ADAM parameters
+"""# ADAM parameters
 rho1 = 0.99
 rho2 = 0.999
-delta = 1e-10  # small constant to avoid division by zero
-
+delta = 1e-10  # small constant to avoid division by zero"""
 
 # Grid plot parameters
 eta_vals = np.logspace(-5, -1, 5)  # learning rates
 lmbd_vals = np.logspace(-5, 0, 6)  # regularization rates
 
-# Create data set
-rng = np.random.default_rng(rng_seed)
-x = rng.uniform(-xmax, xmax, size=(n, 1))  # .reshape(-1, 1)
-noise = rng.normal(0, noise_std, x.shape)
-y = 2 + 3 * x + 4 * x**2 + noise
+# Set up design matrix and target vector
+data = load_breast_cancer()
+X = data.data
+target = data.target
 
+# Split data into training and testing set
+X_train, X_test, y_train, y_test  = train_test_split(X, target, test_size=0.2,
+                                                        random_state=rng_seed)
+
+# Set up random number generator
+rng = np.random.default_rng(rng_seed)
+
+# Set up gradient function
+train_cost = cost_logreg(y_train.ravel())    
+cost_grad = grad(train_cost)
 
 def create_X_1d(x, n):
     """Returns the design matrix X from coordinates x with n polynomial degrees."""
@@ -47,52 +59,40 @@ def create_X_1d(x, n):
     return X
 
 
-x_test, x_train, y_test, y_train = train_test_split(x, y, test_size=0.2,
-                                                    random_state=rng_seed)
+def stochastic_gradient_descent_logreg(X, y, eta, n_batches, lmbda=0,
+                             max_epochs=1000, tol=1e-7):
+    """Stochastic minibatch gradient descent algorithm"""
+    M = int(n/n_batches)  # size of minibatches
+    
+    # Start with random theta guess
+    theta = rng.standard_normal((2, 1))
 
-test_cost = cost_ols(y_test.ravel())                                                   
+    for epoch in max_epochs:
+        # Iterate through all minibatches for each epoch iteration
+        for i in range(n_batches):
+            # Pick a minibatch at random from the data set 
+            random_index = M * rng.integers(m)
+            Xi = X[random_index:random_index + M]
+            yi = y[random_index:random_index + M]
+            
+            # Calculate logistic regression gradient
+            gradient = cost_grad(Xi, yi, lmbda, theta)
 
-X_train = create_X_1d(x_train, degree)
-X_test = create_X_1d(x_test, degree)
+            # Update theta
+            theta_prev = theta.copy()
+            theta -= eta * gradient
 
-theta_adamsgd_ridge_prev = np.zeros(theta_adamsgd_ridge.shape)
-for epoch_ridge in epoch_iters:
-    theta_adamsgd_ridge_prev = theta_adamsgd_ridge.copy()
-    moment1 = 0
-    moment2 = 0
+        # Convergence test
+        if all(abs(theta - theta_prev) <= tol):
+            return theta
 
-    # Iterate through all minibatches for each epoch iteration
-    for i in range(m):
-        # Pick a minibatch at random from the data set 
-        random_index = M * rng.integers(m)
-        Xi = X[random_index:random_index + M]
-        yi = y[random_index:random_index + M]
-        
-        # Calculate gradient
-        gradient = grad_ridge(Xi, yi, lmbda, theta_adamsgd_ridge)
-        
-        # Update moments
-        moment1 = rho1 * moment1 + (1 - rho1) * gradient
-        moment2 = rho2 * moment2 + (1 - rho2) * gradient * gradient
-        
-        # Correct moment bias'
-        term1 = moment1/(1 - rho1**(epoch_ridge+1))
-        term2 = moment2/(1 - rho2**(epoch_ridge+1))
+    return theta
 
-        # Update theta
-        theta_adamsgd_ridge -= learn_rate*term1 / (delta + np.sqrt(term2))  # scale learning rate with r
 
-    # Store MSE as error
-    errors_adamsgd_ridge.append(ols_cost(X, y, theta_adamsgd_ridge))
+test_cost = cost_logreg(y_test.ravel())                                                   
 
-    # Convergence test
-    if all(abs(theta_adamsgd_ridge - theta_adamsgd_ridge_prev) <= theta_tol):
-        break
+print()
 
-plt.plot(errors_adamsgd_ols, label="OLS")
-plt.plot(errors_adamsgd_ridge, label=f"Ridge, lambda={lmbda}")
-plt.xlabel("Iterations")
-plt.ylabel("Error")
-plt.title(rf"ADAM SGD, $\eta_0={learn_rate:.2f}, \rho_1={rho1}, \rho_2={rho2}, {M=}$")
-plt.grid(True)
-plt.legend()
+# plt.grid(True)
+# plt.legend()
+# plt.savefig("../results/figures/part_e_log_reg.png")
