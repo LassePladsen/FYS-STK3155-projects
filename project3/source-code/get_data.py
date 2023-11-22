534a385f317739ef779bf7c40434cb3f6b2daa88
Rogumann <lasse.pladsen@hotmail.com>
2023-11-22 13:37:59 +0100

    added smote oversampling and resampling

--- project3/source-code/get_data.py
+++ project3/source-code/get_data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from imblearn.over_sampling import SMOTE
+from sklearn.model_selection import train_test_split
 
 
 def get_exoplanet_data(
@@ -17,3 +19,14 @@ def get_exoplanet_data(
 
     return x_train, x_test, y_train, y_test
 
+
+def get_balanced_exoplanet_data(
+    trainfile: str = "../data/exoTrain.csv", testfile: str = "../data/exoTest.csv"
+) -> (pd.DataFrame, pd.DataFrame, pd.Series, pd.Series):
+    """Returns the exoplanet training and testing data from the data folder oversampled using the SMOTE algorithm from imblearn, then split into new train and test split using scikit-learns' train_test_split"""
+    x_train, x_test, y_train, y_test = get_exoplanet_data(trainfile, testfile)
+
+    # SMOTE AND SPLIT
+    smote = SMOTE()
+    x_train_smote, y_train_smote = smote.fit_resample(x_train, y_train)
+    return train_test_split(x_train_smote, y_train_smote)
