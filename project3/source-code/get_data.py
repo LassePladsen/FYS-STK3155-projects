b2f0a94f92b1fcea3a75a67da32eb41c400345d7
Rogumann <lasse.pladsen@hotmail.com>
2023-11-23 11:51:12 +0100

    added scaling function

--- project3/source-code/get_data.py
+++ project3/source-code/get_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 
 def get_raw_exoplanet_data(
@@ -30,3 +31,15 @@ def get_balanced_exoplanet_data(
     smote = SMOTE()
     x_train_smote, y_train_smote = smote.fit_resample(x_train, y_train)
     return train_test_split(x_train_smote, y_train_smote, test_size=0.2)
+
+
+def get_scaled_balanced_exoplanet_data(
+    trainfile: str = "../data/exoTrain.csv", testfile: str = "../data/exoTest.csv"
+) -> (pd.DataFrame, pd.DataFrame, pd.Series, pd.Series):
+    """Returns the exoplanet training and testing data from the data folder oversampled using the SMOTE algorithm from imblearn, then split into new train and test split using scikit-learns' train_test_split, and finally scaled using scikit-learn's StandardScaler"""
+    x_train, x_test, y_train, y_test = get_balanced_exoplanet_data(trainfile, testfile)
+
+    scaler = StandardScaler()
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_test_scaled = scaler.transform(x_test)
+    return x_train_scaled, x_test_scaled, y_train, y_test
