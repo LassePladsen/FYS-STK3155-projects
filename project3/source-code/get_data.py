e989fd0ed6f5d563f51eb5b2f686bfc2f020554d
Rogumann <lasse.pladsen@hotmail.com>
2023-11-23 11:18:04 +0100

    renamed raw data function

--- project3/source-code/get_data.py
+++ project3/source-code/get_data.py
@@ -3,7 +3,7 @@ from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
 
 
-def get_exoplanet_data(
+def get_raw_exoplanet_data(
     trainfile: str = "../data/exoTrain.csv", testfile: str = "../data/exoTest.csv"
 ) -> (pd.DataFrame, pd.DataFrame, pd.Series, pd.Series):
     """Returns the exoplanet training and testing data from the data folder"""
@@ -24,7 +24,7 @@ def get_balanced_exoplanet_data(
     trainfile: str = "../data/exoTrain.csv", testfile: str = "../data/exoTest.csv"
 ) -> (pd.DataFrame, pd.DataFrame, pd.Series, pd.Series):
     """Returns the exoplanet training and testing data from the data folder oversampled using the SMOTE algorithm from imblearn, then split into new train and test split using scikit-learns' train_test_split"""
-    x_train, x_test, y_train, y_test = get_exoplanet_data(trainfile, testfile)
+    x_train, x_test, y_train, y_test = get_raw_exoplanet_data(trainfile, testfile)
 
     # SMOTE AND SPLIT
     smote = SMOTE()
